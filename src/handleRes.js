a877a55b00bceaafd37934ee4ae33e2f12e0f4a5
Philippe Larochelle <phil@emiphil.com>
2018-03-15 11:58:15 -0600

    Add proceed() comment skeleton

--- src/handleRes.js
+++ src/handleRes.js
@@ -1,3 +1,9 @@
-function handleRes () { }
+function handleRes () {
+  // check if we have already responded to the request
+
+  // if we have, send a warning log and do nothing
+
+  // otherwise, return true
+}
 
 module.exports = handleRes
